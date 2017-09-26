6bf33a33c682468aea59f7a1649c868432955888
clarlars <clarlars@gmail.com>
2017-09-25 18:02:01 -0700

    First pass at UploadFileServlet and XLSXConverter

---
+++ webservice/src/org/opendatakit/webservice/rest/fileio/UploadFileServlet.java
@@ -0,0 +1,40 @@
+package org.opendatakit.webservice.rest.fileio;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/OdkFilesIf/*", asyncSupported = true)
+public class UploadFileServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  static final String TAG = "UploadFileServlet";
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public UploadFileServlet() {
+    super();
+  }
+
+  /**
+   * Upload file
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    
+    AsyncContext asyncCtxt = request.startAsync();
+    
+    UploadFileAsyncListener action = new UploadFileAsyncListener(asyncCtxt);
+    
+    asyncCtxt.addListener(action);
+    asyncCtxt.setTimeout(90000L);
+  }
+}
